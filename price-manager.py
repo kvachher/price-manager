8ba3db2b3442990c42967363bc87de7d841c139a
Kush Vachher <kushvachher@Kushs-MacBook-Air.local>
2023-10-26 18:24:09 -0400

    wrote very basic script

---
+++ price-manager.py
@@ -0,0 +1,32 @@
+dict = {}
+
+def create_dict(ppl) : 
+    for p in ppl : 
+        dict[p] = 0
+
+def refresh_prices(ppl) : 
+    for p in ppl : 
+        dict[p] = 0
+
+def det_split(price, splitters) : 
+    per_person = price / len(splitters)
+    for p in dict : 
+        dict[p] += per_person
+
+def print_results() : 
+    for d in dict: 
+        print(f'{d} owes {str(dict[d])}')
+
+## sample usage - first create list of people splitting
+l = ["kush", "om", "kaushal", "mit"]
+
+## call 'create_dict' -> this will keep track of prices
+create_dict(l)
+
+## create a list of people splitting an item - let's say all four of us are splitting eggs
+egg_splitters = l # set it to same list as all roommates
+
+## call det_split to fix prices. enter egg price here as well.
+det_split(5, egg_splitters)
+
+print_results()
